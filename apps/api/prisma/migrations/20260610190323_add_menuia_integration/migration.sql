d6f04c6635a42d9ae3f9ceb207fa9c7dcb9bab30
guiguidogogo <30851728+guiguidogogo@users.noreply.github.com>
2026-06-13 09:56:55 -0300

    Corrige nome da tabela Setting na migração MySQL

--- apps/api/prisma/migrations/20260610190323_add_menuia_integration/migration.sql
+++ apps/api/prisma/migrations/20260610190323_add_menuia_integration/migration.sql
@@ -1,4 +1,4 @@
 -- AlterTable
-ALTER TABLE `setting` ADD COLUMN `menuiaApiKey` VARCHAR(191) NULL,
+ALTER TABLE `Setting` ADD COLUMN `menuiaApiKey` VARCHAR(191) NULL,
     ADD COLUMN `menuiaEnabled` BOOLEAN NOT NULL DEFAULT false,
     ADD COLUMN `menuiaStoreId` VARCHAR(191) NULL;
